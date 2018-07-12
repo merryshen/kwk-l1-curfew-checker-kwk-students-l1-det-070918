f858614ea4467f65b03548700b582cedea5c1fa8
merryshen <yihuaca@gmail.com>
2018-07-12 13:19:01 +0000

    automatically backed up by learn

--- curfew_checker.rb
+++ curfew_checker.rb
@@ -1,8 +1,8 @@
 def curfewchecker(time)
   curfew = 11
-  if time - curfew > 0
+  if curfew > 0
     puts "You're in trouble! Better get home quick!"
-  elsif time - curfew == 0
+  elsif curfew == 0
     puts "Time to apparate!!"
   else
     puts "You have #{curfew-time} hour(s) left to keep having fun!"
